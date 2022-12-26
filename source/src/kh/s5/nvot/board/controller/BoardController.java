7c7c7285c74fa243d5f1c771c99b00b35eb4ef68
MinJiPark <pminji1@gmail.com>
2022-12-26 15:40:51 +0900

    Board 에 대한 Controller 작성

---
+++ source/src/kh/s5/nvot/board/controller/BoardController.java
@@ -0,0 +1,36 @@
+package kh.s5.nvot.board.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BoardController
+ */
+@WebServlet("/Board")
+public class BoardController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String viewPath = "WEB-INF/view/member/board.jsp";
+		request.getRequestDispatcher(viewPath).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+}
