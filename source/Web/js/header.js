da9af6bc1123b5cf0fa8f26e90a74371d1b9e2fe
MinJiPark <pminji1@gmail.com>
2022-12-28 21:36:17 +0900

    jquery, header js 파일 업로드

---
+++ source/Web/js/header.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $(".slider-banner-511517418")
+      .not(".slick-initialized")
+      .slick({
+        autoplay: true,
+        dots: false,
+        infinite: true,
+        autoplaySpeed: 3000,
+        speed: 1300,
+        slidesToShow: 1,
+        fade: true,
+        centerMode: true,
+        variableWidth: true,
+        adaptiveHeight: true,
+        draggable: false,
+      });
+  });
