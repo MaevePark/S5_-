53dd28c25b30384fafad81dd27a5ef5a5dd317bc
MinJiPark <pminji1@gmail.com>
2022-12-28 21:45:54 +0900

    MainController 일부 수정
    ProductService, ProductVo 추가

--- source/src/kh/s5/nvot/main/controller/MainController.java
+++ source/src/kh/s5/nvot/main/controller/MainController.java
@@ -1,11 +1,16 @@
 package kh.s5.nvot.main.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import kh.s5.nvot.product.model.ProductService;
+import kh.s5.nvot.product.model.ProductVo;
 
 /**
  * Servlet implementation class MainController
@@ -25,6 +30,10 @@ public class MainController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ProductService service = new ProductService();
+		List<ProductVo> volist = service.selectList();
+		System.out.println(volist);
+		request.setAttribute("productlist", volist);
 		String viewPath = "WEB-INF/view/member/index.jsp";
 		request.getRequestDispatcher(viewPath).forward(request, response);
 	}
