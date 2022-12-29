ac257c81990c297c770de6cbc560fd22dad20aa2
MinJiPark <pminji1@gmail.com>
2022-12-30 00:30:31 +0900

    login, logout, logindo 에 대한 controller 작성

---
+++ source/src/kh/s5/nvot/member/controller/LoginDoController.java
@@ -0,0 +1,61 @@
+package kh.s5.nvot.member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kh.s5.nvot.member.model.MemberService;
+import kh.s5.nvot.member.model.MemberVo;
+
+
+/**
+ * Servlet implementation class LoginDoController
+ */
+@WebServlet("/login.do")
+public class LoginDoController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginDoController() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String member_id = request.getParameter("member_id");
+		String member_pwd = request.getParameter("member_pwd");
+		
+		MemberService service = new MemberService();
+		MemberVo loginInfo = service.login(member_id, member_pwd);
+		
+		if(loginInfo !=null) {
+			System.out.println("로그인 성공");
+			request.getSession().setAttribute("loginSsInfo", loginInfo);
+			response.sendRedirect(request.getContextPath()+"/main");
+		} else {
+			System.out.println("로그인 실패");
+			response.sendRedirect(request.getContextPath()+"/login");		
+		} 
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- source/src/kh/s5/nvot/member/controller/LogoutController.java
+++ source/src/kh/s5/nvot/member/controller/LogoutController.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class LoginController
+ * Servlet implementation class LogoutController
  */
-@WebServlet("/login")
-public class LoginController extends HttpServlet {
+@WebServlet("/logout")
+public class LogoutController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public LoginController() {
+    public LogoutController() {
         super();
     }
 
@@ -25,7 +25,9 @@ public class LoginController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String viewPath = "WEB-INF/view/member/login.jsp";
-		request.getRequestDispatcher(viewPath).forward(request, response);
+		request.getSession().invalidate(); 
+		response.sendRedirect(request.getContextPath()+"/main");
 	}
+
+
 }
