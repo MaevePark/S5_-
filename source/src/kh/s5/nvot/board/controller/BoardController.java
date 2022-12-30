379ecd2e5fae7a3dd072835a5cc63587d52c4e1b
MinJiPark <pminji1@gmail.com>
2022-12-30 22:48:44 +0900

    대소문자 수정

--- source/src/kh/s5/nvot/board/controller/BoardController.java
+++ source/src/kh/s5/nvot/board/controller/BoardController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class BoardController
  */
-@WebServlet("/Board")
+@WebServlet("/board")
 public class BoardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
