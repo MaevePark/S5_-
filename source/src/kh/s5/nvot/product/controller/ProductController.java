8ecc0bd030e9fdea53199a4c213a963c28f1e24d
MinJiPark <pminji1@gmail.com>
2023-01-02 22:31:16 +0900

    Product 에 대한 Controller 작성

---
+++ source/src/kh/s5/nvot/product/controller/ProductController.java
@@ -0,0 +1,33 @@
+package kh.s5.nvot.product.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ProductController
+ */
+@WebServlet("/product")
+public class ProductController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductController() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String viewPath = "WEB-INF/view/member/product.jsp";
+		request.getRequestDispatcher(viewPath).forward(request, response);
+		
+		
+	}
+}
