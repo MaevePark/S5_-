4aaa93df78e1454e3620b7c6db85f3346c721512
MinJiPark <pminji1@gmail.com>
2023-01-02 23:15:08 +0900

    BoardFile 에 대한 Vo 일부 수정
    대소문자 수정

--- source/src/kh/s5/nvot/board/model/BoardFileVo.java
+++ source/src/kh/s5/nvot/board/model/BoardFileVo.java
@@ -3,25 +3,30 @@ package kh.s5.nvot.board.model;
 public class BoardFileVo {
 //	BOARD_FILEPATH NOT NULL VARCHAR2(500) 
 //	BOARD_NO       NOT NULL NUMBER    
-	private String BOARD_FILEPATH;
-	private int BOARD_NO;
+	private String board_filepath;
+	private int board_no;
+	public BoardFileVo() {
+		super();
+	}
+	public BoardFileVo(String board_filepath, int board_no) {
+		super();
+		this.board_filepath = board_filepath;
+		this.board_no = board_no;
+	}
 	@Override
 	public String toString() {
-		return "BoardFileVo [BOARD_FILEPATH=" + BOARD_FILEPATH + ", BOARD_NO=" + BOARD_NO + "]";
+		return "BoardFileVo [board_filepath=" + board_filepath + ", board_no=" + board_no + "]";
 	}
-	public String getBOARD_FILEPATH() {
-		return BOARD_FILEPATH;
+	public String getBoard_filepath() {
+		return board_filepath;
 	}
-	public void setBOARD_FILEPATH(String bOARD_FILEPATH) {
-		BOARD_FILEPATH = bOARD_FILEPATH;
+	public void setBoard_filepath(String board_filepath) {
+		this.board_filepath = board_filepath;
 	}
-	public int getBOARD_NO() {
-		return BOARD_NO;
+	public int getBoard_no() {
+		return board_no;
 	}
-	public void setBOARD_NO(int bOARD_NO) {
-		BOARD_NO = bOARD_NO;
+	public void setBoard_no(int board_no) {
+		this.board_no = board_no;
 	}
-
-	
-	
 }
